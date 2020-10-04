df120df6fdfe88c965281f02d00523fd6af746b3
Rafael <suspect567@gmail.com>
2020-10-03 19:43:35 -0500

    Chat final

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,20 +1,27 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -24,21 +31,30 @@ socket.on('disconnect', function() {
 
 });
 
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//Lista los usuarios activos de un chat-entrada y salida
-socket.on('listaPersonas', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersonas', function(personas) {
+    renderizarUsuarios(personas);
 });
 
-socket.on('mensajeProvado', function(mensaje) {
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('mensaje privado', mensaje);
+    console.log('Mensaje Privado:', mensaje);
+
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -18,19 +18,23 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSalas(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió al chat`));
         callback(usuarios.getPersonasPorSalas(data.sala));
 
 
     })
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id)
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
     client.on('disconnect', () => {
         let personaborrada = usuarios.borrarPersona(client.id);
         client.broadcast.to(personaborrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaborrada.nombre} abandonó el chat`));
         client.broadcast.to(personaborrada.sala).emit('listaPersonas', usuarios.getPersonasPorSalas(personaborrada.sala));
+
     });
 
     client.on('mensajePrivado', (data) => {
